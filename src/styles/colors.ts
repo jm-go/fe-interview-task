8ff44493c58dc1b44565a662a8f56784a3eeeff9
jm-go <jowita.m.gogol@gmail.com>
2025-10-19 15:03:46 +0100

    feat: Added colors sheet

---
+++ src/styles/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  background: "#FFFFFF",
+  headerBackground: "#0B2545",
+
+  headerText: "#FFFFFF",
+
+  purple: "#7C3AED",
+  purpleHover: "#6D28D9",
+
+  textPrimary: "#1F2937",
+  textSecondary: "#6B7280",
+
+  red: "#DC2626",
+  redHover: "#B91C1C",
+};
