805b2945d9eb8972269b50eda71d5b4626f1c619
jm-go <jowita.m.gogol@gmail.com>
2025-10-19 17:42:44 +0100

    feat: Added DetailsView component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Header } from "./header/Header";
+import { Header, type HeaderProps } from "./header/Header";
 import "../styles/global.css";
 
 type LayoutProps = {
   children: React.ReactNode;
   showHeader?: boolean;
+  headerProps?: HeaderProps;
 };
 
-export const Layout: React.FC<LayoutProps> = ({ children, showHeader = true }) => (
-  <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-    {showHeader && <Header />}
+export const Layout: React.FC<LayoutProps> = ({
+  children,
+  showHeader = true,
+  headerProps,
+}) => (
+  <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+    {showHeader && <Header {...headerProps} />}
     <main className="app-container">{children}</main>
   </div>
 );
